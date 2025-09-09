e8ed7239a807bf31dbc548c0e13886fb608ee610
Lidiya <torlovalidia@gmail.com>
2025-09-09 12:43:30 +0300

    изменен файл readme

--- pages/account.vue
+++ pages/account.vue
@@ -13,7 +13,9 @@ const handleLogout = () => {
   <div
     class="max-w-[1045px] mx-auto 2xl:py-20 s:py-10 s:px-3 flex flex-col gap-9 items-start justify-center"
   >
-    <h2 class="text-6xl s:text-[1.8rem] font-bold">Привет, {{ user?.name }}</h2>
+    <h2 class="2xl:text-6xl s:text-[1.8rem] font-bold">
+      Привет, {{ user?.name }}
+    </h2>
     <div
       class="2xl:w-[45%] s:w-full sm:w-1/2 h-12 flex sm:flex-row s:flex-col gap-3"
     >
